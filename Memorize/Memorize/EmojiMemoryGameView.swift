08c579d2591d512302966fed310d32d040dd132c
Thiago Martins <thiago.martins@foton.la>
2021-05-19 22:56:32 -0300

    Implementing access control in the main memorize game

--- Memorize/Memorize/EmojiMemoryGameView.swift
+++ Memorize/Memorize/EmojiMemoryGameView.swift
@@ -35,7 +35,7 @@ struct CardView: View {
         }
     }
     
-    func body(for size: CGSize) -> some View {
+    private func body(for size: CGSize) -> some View {
         ZStack {
             if card.isFaceUp {
                 RoundedRectangle(cornerRadius: cornerRadius).fill(Color.white)
@@ -52,10 +52,10 @@ struct CardView: View {
     
     // MARK: - Drawing Constants:
     
-    let cornerRadius: CGFloat = 10
-    let edgeLineWidth: CGFloat = 3
+    private let cornerRadius: CGFloat = 10
+    private let edgeLineWidth: CGFloat = 3
     
-    func fontSize(for size: CGSize) -> CGFloat {
+    private func fontSize(for size: CGSize) -> CGFloat {
         min(size.width, size.height) * 0.75
     }
     
