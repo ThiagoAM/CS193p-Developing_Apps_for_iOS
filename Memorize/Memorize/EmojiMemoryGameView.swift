a84417ed63851bed59f12373fc29be412d498f6f
Thiago Martins <thiago.martins@foton.la>
2021-05-29 01:36:10 -0300

    Creating the Pie shap

--- Memorize/Memorize/EmojiMemoryGameView.swift
+++ Memorize/Memorize/EmojiMemoryGameView.swift
@@ -40,6 +40,11 @@ struct CardView: View {
             if card.isFaceUp {
                 RoundedRectangle(cornerRadius: cornerRadius).fill(Color.white)
                 RoundedRectangle(cornerRadius: cornerRadius).stroke(lineWidth: edgeLineWidth)
+                
+                Pie(startAngle: Angle.degrees(0 - 90), endAngle: Angle.degrees(110 - 90), clockwise: true)
+                    .padding(5)
+                    .opacity(0.4)
+                
                 Text(card.content)
             } else {
                 if !card.isMatched {
@@ -56,7 +61,7 @@ struct CardView: View {
     private let edgeLineWidth: CGFloat = 3
     
     private func fontSize(for size: CGSize) -> CGFloat {
-        min(size.width, size.height) * 0.75
+        min(size.width, size.height) * 0.7
     }
     
 }
@@ -85,6 +90,8 @@ struct CardView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        EmojiMemoryGameView(viewModel: EmojiMemoryGame())
+        let game = EmojiMemoryGame()
+        game.choose(card: game.cards[0])
+        return EmojiMemoryGameView(viewModel: game)
     }
 }
---
+++ Memorize/Memorize/Pie.swift
@@ -0,0 +1,41 @@
+//
+//  Pie.swift
+//  Memorize
+//
+//  Created by Thiago Martins on 29/05/21.
+//
+
+import SwiftUI
+
+struct Pie: Shape {
+    
+    var startAngle: Angle
+    var endAngle: Angle
+    var clockwise: Bool = false
+    
+    func path(in rect: CGRect) -> Path {
+        
+        let center = CGPoint(x: rect.midX, y: rect.midY)
+        let radius = min(rect.midX, rect.midY)
+        let start = CGPoint(
+            x: center.x + radius * cos(CGFloat(startAngle.radians)),
+            y: center.y + radius * sin(CGFloat(startAngle.radians))
+        )
+        
+        var p = Path()
+        
+        p.move(to: center)
+        p.addLine(to: start)
+        p.addArc(
+            center: center,
+            radius: radius,
+            startAngle: startAngle,
+            endAngle: endAngle,
+            clockwise: clockwise
+        )
+        p.addLine(to: center)
+        
+        return p
+    }
+    
+}
